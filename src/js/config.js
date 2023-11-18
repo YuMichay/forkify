570b8293929def1ccdaf804fe9bfbea49ccddd18
Yuliya Kudrautsava <julia.one.such@gmail.com>
2023-11-18 19:17:10 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_KEY = 'bbee5106-10a8-45cb-b3f9-096ff7435da4';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 15;
+export const MODAL_CLOSE_SEC = 2.5;

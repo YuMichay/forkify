c78eadf8bc9d1128eb6694e14cacebee39164207
Yuliya Kudrautsava <julia.one.such@gmail.com>
2023-11-18 19:47:04 +0100

    Delete fraction

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -1,5 +1,4 @@
 import icons from "../../img/icons.svg";
-import { Fraction } from "fractional";
 import View from "./View";
 
 class RecipeView extends View {
@@ -116,7 +115,7 @@ class RecipeView extends View {
                 <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ""}</div>
+                <div class="recipe__quantity">${ingredient.quantity ? ingredient.quantity.toString() : ""}</div>
                 <div class="recipe__description">
                 <span class="recipe__unit">${ingredient.unit}</span>
                 ${ingredient.description}
